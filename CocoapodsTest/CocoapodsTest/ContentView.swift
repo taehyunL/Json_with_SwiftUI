42d82d51d46eebd245f17864ba2730a497de0c0c
taehyunL <taylor1233@naver.com>
2021-12-22 22:55:44 +0900

    네트워크 호출

--- CocoapodsTest/CocoapodsTest/ContentView.swift
+++ CocoapodsTest/CocoapodsTest/ContentView.swift
@@ -25,9 +25,8 @@ class ViewModel: ObservableObject {
             [weak self] data, _, error in
             guard let data = data, error == nil else {
                 return
-            }//옵셔널 언래핑 해줘야함 조건 안맞으면 return
+            }
             
-            //data가 존재한다면 이제 디코딩을 해줘야함
             do {
                 let courses = try JSONDecoder().decode([Course].self, from: data)
                 DispatchQueue.main.async {
@@ -35,7 +34,7 @@ class ViewModel: ObservableObject {
                 }
             } catch {
                 print(error)
-            }//오류메세지
+            }
         }
         task.resume()
     }
@@ -61,7 +60,7 @@ struct ContentView: View {
             .navigationTitle("조건")
             .onAppear {
                 viewModel.fetch()
-            }//onAppear를 통해여 호출해줘야함
+            }
         }
     }
 }
