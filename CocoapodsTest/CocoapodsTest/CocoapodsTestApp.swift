88358991872a7100985025c1312d76bd5e548d8b
taehyunL <taylor1233@naver.com>
2021-12-22 19:06:44 +0900

    initial App

---
+++ CocoapodsTest/CocoapodsTest/CocoapodsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoapodsTestApp.swift
+//  CocoapodsTest
+//
+//  Created by 이태현 on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CocoapodsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
